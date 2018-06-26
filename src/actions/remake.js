186fca41c2760ca52efc3c90219add55a8d9c70a
Falk Benke <benke.falk@gmail.com>
2018-06-26 18:19:38 +0100

    Reset tmdb info

--- src/actions/remake.js
+++ src/actions/remake.js
@@ -5,6 +5,7 @@ export const CREATE_REMAKE = 'create_remake'
 export const FETCH_REMAKES = 'fetch_remakes'
 export const FETCH_REMAKE = 'fetch_remake'
 export const DELETE_REMAKE = 'delete_remake'
+export const RESET_TMDB_DETAILS = 'reset_tmdb_details'
 
 export const REMAKE_URL = `${process.env.API_URL}api/remakes/`
 
@@ -32,6 +33,7 @@ export function createRemake (values, callback) {
 
 export function fetchRemake (id) {
   return function (dispatch) {
+    dispatch({type: RESET_TMDB_DETAILS})
     axios.get(`${REMAKE_URL}${id}`)
       .then(response => {
         dispatch({
--- src/reducers/tmdb_info.js
+++ src/reducers/tmdb_info.js
@@ -4,11 +4,15 @@ import {
   FETCH_ADDITIONAL_MOVIE_INFO
 } from '../actions/movie'
 
+import { RESET_TMDB_DETAILS } from '../actions/remake'
+
 export default function (state = {}, action) {
   switch (action.type) {
     case SET_MOVIE_ID:
       return {}
     case RESET_MOVIE_SUGGESTIONS:
+      return {}
+    case RESET_TMDB_DETAILS:
       return {}
     case FETCH_ADDITIONAL_MOVIE_INFO:
       return action.payload.data
