4abda8466e7779020ce3d5598fd6121f4506b8f7
Falk Benke <benke.falk@gmail.com>
2018-07-03 12:14:12 +0200

    Add jumbotron to home

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
 const Home = () => {
-  return <div> Welcome to Fantasy Cast </div>
+  return (
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Fantasy Cast</h1>
+        <p class="lead">Predict who is going to get famous roles in upcoming movies.</p>
+      </div>
+    </div>
+  )
 }
 
 export default Home
