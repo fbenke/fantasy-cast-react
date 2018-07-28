8bc64e9434ffd489a633ca95ee7145fc10a5bd9c
Falk Benke <benke.falk@gmail.com>
2018-07-28 14:27:48 +0200

    Apply camelcase to reducer files

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,9 @@ import { reducer as formReducer } from 'redux-form';
 import AuthenticationReducer from './authentication';
 import RemakesReducer from './remakes';
 import RemakeReducer from './remake';
-import NewRemakeReducer from './new_remake';
-import MovieReducer from './movie_suggestions';
-import TmdbInfoReducer from './tmdb_info';
+import NewRemakeReducer from './newRemake';
+import MovieReducer from './movieSuggestions';
+import TmdbInfoReducer from './tmdbInfo';
 
 const rootReducer = combineReducers({
   form: formReducer,
